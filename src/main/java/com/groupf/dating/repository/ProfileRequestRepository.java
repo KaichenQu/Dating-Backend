65c376688dd6d3814d33cd732a760ecf4f5e35eb
Yihan Wang <arinayih@gmail.com>
2026-03-18 18:05:50 -0700

    refactor(ProfileRequestRepository): swap MongoRepository for JpaRepository

--- src/main/java/com/groupf/dating/repository/ProfileRequestRepository.java
+++ src/main/java/com/groupf/dating/repository/ProfileRequestRepository.java
@@ -1,13 +1,14 @@
 package com.groupf.dating.repository;
 
 import com.groupf.dating.model.ProfileOptimizationRequest;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface ProfileRequestRepository extends MongoRepository<ProfileOptimizationRequest, String> {
+public interface ProfileRequestRepository extends JpaRepository<ProfileOptimizationRequest, UUID> {
 
     List<ProfileOptimizationRequest> findByUserId(String userId);
 }
